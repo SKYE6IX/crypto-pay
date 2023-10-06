7d7cbb80f60b00ec28fcf4f761ab20189f930a80
SKYE <skye@MacBook-Pro-MacBook.local>
2023-10-06 13:37:35 +0300

    Added footer block

--- src/modules/community-block/CommunityBlock.tsx
+++ src/modules/community-block/CommunityBlock.tsx
@@ -9,37 +9,39 @@ import './styles/community-block.scss';
 const CommunityBlock = () => {
   return (
     <section className="community-block">
-      <div className="community-block__header-text">
-        <h2>Join dev community</h2>
-        <p>
-          Connect with bot developers and explore the possibilities <br />
-          our crypto payments service unlocks.
-        </p>
-      </div>
-      <div className="community-block__icons">
-        <div className="community-block__icons-wrapper">
-          <Image src={JsIcon} alt="js-icon" />
-          <span>crypto-pay-api</span>
+      <div className="community-block__inner-container">
+        <div className="community-block__header-text">
+          <h2>Join dev community</h2>
+          <p>
+            Connect with bot developers and explore the possibilities <br />
+            our crypto payments service unlocks.
+          </p>
         </div>
-        <div className="community-block__icons-wrapper">
-          <Image src={NetIcon} alt="net-icon" />
-          <span>CryptoPay</span>
+        <div className="community-block__icons">
+          <div className="community-block__icons-wrapper">
+            <Image src={JsIcon} alt="js-icon" />
+            <span>crypto-pay-api</span>
+          </div>
+          <div className="community-block__icons-wrapper">
+            <Image src={NetIcon} alt="net-icon" />
+            <span>CryptoPay</span>
+          </div>
+          <div className="community-block__icons-wrapper">
+            <Image src={PhpIcon} alt="php-icon" />
+            <span>crypto-pay-api</span>
+          </div>
+          <div className="community-block__icons-wrapper">
+            <Image src={PythonIcon} alt="pyhton-icon" />
+            <span>aiocryptopay</span>
+          </div>
+          <div className="community-block__icons-wrapper">
+            <Image src={GoIcon} alt="go-icon" />
+            <span>cryptobot-sdk-golang</span>
+          </div>
         </div>
-        <div className="community-block__icons-wrapper">
-          <Image src={PhpIcon} alt="php-icon" />
-          <span>crypto-pay-api</span>
+        <div className="community-block__button">
+          <button>Open Devs chat</button>
         </div>
-        <div className="community-block__icons-wrapper">
-          <Image src={PythonIcon} alt="pyhton-icon" />
-          <span>aiocryptopay</span>
-        </div>
-        <div className="community-block__icons-wrapper">
-          <Image src={GoIcon} alt="go-icon" />
-          <span>cryptobot-sdk-golang</span>
-        </div>
-      </div>
-      <div className="community-block__button">
-        <button>Open Devs chat</button>
       </div>
     </section>
   );
--- src/modules/footer-block/Footer.tsx
+++ src/modules/footer-block/Footer.tsx
@@ -4,23 +4,25 @@ import './syles/footer.scss';
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer__header-text">
-        <div className="footer__logo-wrapper">
-          <Image src={cryptoLogo} alt="crypto-logo" />
+      <div className="footer__inner-container">
+        <div className="footer__headers">
+          <div className="footer__logo-wrapper">
+            <Image src={cryptoLogo} alt="crypto-logo" />
+          </div>
+          <div className="footer__nav-menu-wrapper">
+            <ul className="footer__nav-menu_list">
+              <li>Use cases</li>
+              <li>Features</li>
+              <li>How to start</li>
+            </ul>
+          </div>
+          <div className="footer__button-wrapper">
+            <button>Get started</button>
+          </div>
         </div>
-        <div className="footer__nav-menu-wrapper">
-          <ul className="header__nav-menu_list">
-            <li>Use cases</li>
-            <li>Features</li>
-            <li>How to start</li>
-          </ul>
+        <div className="footer__copyright">
+          <span>©2023 Crypto Bot</span>
         </div>
-        <div className="footer__button-wrapper">
-          <button className="footer__button">Get started</button>
-        </div>
-      </div>
-      <div className="footer__copyright">
-        <span>©2023 Crypto Bot</span>
       </div>
     </footer>
   );
