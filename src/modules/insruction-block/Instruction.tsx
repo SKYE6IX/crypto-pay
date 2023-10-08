fbafd5c0e5c9d4477b3c143ccc95483d4f84c867
SKYE <skye@MacBook-Pro-MacBook.local>
2023-10-08 18:59:47 +0300

    Added breakpoint for instruction block

--- src/modules/insruction-block/Instruction.tsx
+++ src/modules/insruction-block/Instruction.tsx
@@ -15,30 +15,30 @@ const Insruction = () => {
 
       <div className="instruction__step instruction__step-one">
         <div className="instruction__step-header">
-          <div className="instruction__step-header-head">
-            <div className="instruction__step-header-number">
-              <span>1</span>
-            </div>
+          <div className="instruction__step-header-number">
+            <span>1</span>
+          </div>
+          <div className="instruction__step-header-text">
             <div className="instruction__step-header-title">
               <h3>Authorizing your app</h3>
             </div>
-          </div>
-
-          <div className="instruction__step-header-steps-list">
-            <p>
-              1. Open <span>@CryptoBot</span>
-            </p>
-            <p>
-              2. Go to <span>Crypto Pay</span>
-            </p>
-            <p>
-              3. Tap <span>Create App</span>
-            </p>
-            <p>
-              4. Get <span>API Token</span>
-            </p>
+            <div className="instruction__step-header-steps-list">
+              <p>
+                1. Open <span>@CryptoBot</span>
+              </p>
+              <p>
+                2. Go to <span>Crypto Pay</span>
+              </p>
+              <p>
+                3. Tap <span>Create App</span>
+              </p>
+              <p>
+                4. Get <span>API Token</span>
+              </p>
+            </div>
           </div>
         </div>
+
         <div className="instruction__step-image">
           <Image src={StepOneImage} alt="step-one-image" />
         </div>
@@ -46,12 +46,15 @@ const Insruction = () => {
 
       <div className="instruction__step instruction__step-two">
         <div className="instruction__step-header">
-          <div className="instruction__step-header-head">
-            <div className="instruction__step-header-number">
-              <span>2</span>
-            </div>
+          <div className="instruction__step-header-number">
+            <span>2</span>
+          </div>
+          <div className="instruction__step-header-text">
             <div className="instruction__step-header-title">
-              <h3>Crypto Pay API Request</h3>
+              <h3>
+                Crypto Pay <br />
+                API Request
+              </h3>
             </div>
             <div className="instruction__step-header-sub-title">
               <p>
@@ -59,12 +62,12 @@ const Insruction = () => {
                 To pass parameters use:
               </p>
             </div>
-          </div>
-          <div className="instruction__step-header-steps-list">
-            <p>&#8226; URL query string</p>
-            <p>&#8226; application/json </p>
-            <p>&#8226; application/x-www-form-urlencoded</p>
-            <p>&#8226; multipart/form-data</p>
+            <div className="instruction__step-header-steps-list">
+              <p>&#8226; URL query string</p>
+              <p>&#8226; application/json </p>
+              <p>&#8226; application/x-www-form-urlencoded</p>
+              <p>&#8226; multipart/form-data</p>
+            </div>
           </div>
         </div>
         <div className="instruction__step-image">
@@ -74,10 +77,10 @@ const Insruction = () => {
 
       <div className="instruction__step instruction__step-three">
         <div className="instruction__step-header">
-          <div className="instruction__step-header-head">
-            <div className="instruction__step-header-number">
-              <span>3</span>
-            </div>
+          <div className="instruction__step-header-number">
+            <span>3</span>
+          </div>
+          <div className="instruction__step-header-text">
             <div className="instruction__step-header-title">
               <h3>Getting updates</h3>
             </div>
@@ -87,9 +90,6 @@ const Insruction = () => {
                 updates for your app:
               </p>
             </div>
-          </div>
-
-          <div className="instruction__step-header-body">
             <div className="instruction__step-header-steps-list">
               <p>
                 <span>&#8226; getInvoices</span> method to get a list of created{' '}
